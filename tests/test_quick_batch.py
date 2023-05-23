f9844add74de839f4c36b16e2d07702377ecd32a
Jeremy Watt <jermwatt@gmail.com>
2023-05-22 17:00:08 -0700

    tests complete

---
+++ tests/test_quick_batch.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import sys
+import os
+
+base_path = str(Path(__file__).parent.parent)
+sys.path.append(str(base_path))
+
+config_path = f'{base_path}/tests/test_configs/test_quick_batch.yaml'
+processor_path = f'{base_path}/tests/test_processors/processor.py'
+
+# delete contents of f'{base_path}/tests/test_data/output_data' but not folder
+for file_name in os.listdir(f'{base_path}/tests/test_data/output_data'):
+    os.remove(f'{base_path}/tests/test_data/output_data/{file_name}')
+
+print('processor_path: ', processor_path)
+print('config_path: ', config_path)
+print('base_path: ', base_path)
+
+
+def test_quick_batch():
+    # rub quick batch
+    from quick_batch import main
+    main.main(config=config_path,
+              processor=processor_path)
+
+    # load in output data file names from
+    # f'{base_path}/tests/test_data/output_data'
+    output_data = os.listdir(f'{base_path}/tests/test_data/output_data')
+
+    # load in expected output data file names from
+    # f'{base_path}/tests/test_data/expected_output_data'
+    expected_output_data = os.listdir(f'{base_path}/tests/test_data/expected_output_data')
+
+    # loop through each file name in output_data,
+    # assert that it is in expected_output_data
+    for file_name in output_data:
+        assert file_name in expected_output_data
+
+    # loop through each file in output_data, load in contents,
+    # and assert is same as expected
+    for file_name in output_data:
+        with open(f'{base_path}/tests/test_data/output_data/{file_name}', 'r') as f:
+            output_data_contents = f.read()
+        with open(f'{base_path}/tests/test_data/expected_output_data/{file_name}', 'r') as f:
+            expected_output_data_contents = f.read()
+        assert output_data_contents == expected_output_data_contents
