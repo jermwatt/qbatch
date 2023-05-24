d77f2b3a5cd309630721d80b0d725f4a0def2759
Jeremy Watt <jermwatt@gmail.com>
2023-05-24 16:35:47 -0700

    processor image build logs added

--- quick_batch/utilities/manage_images.py
+++ quick_batch/utilities/manage_images.py
@@ -1,9 +1,11 @@
 import shutil
 import os
+import sys
 import docker
 from utilities import queue_path, processor_path
 from .manage_requirements import make_requirements
 from utilities import log_exceptions
+import subprocess
 
 
 # create client for docker
@@ -38,10 +40,71 @@ def build_processor_image(client, requirements_path, processor):
         print('INFO: no valid requirements file, creating from processor')
         make_requirements(processor, container_path)
 
-    # create docker image for processor app - including user defined
-    # requirements
-    client.images.build(path=processor_path, tag='quick_batch_processor_app',
-                        quiet=False)
+    # create docker image for processor app - including user defined    
+    def stream_build_logs(path, tag):
+        command = ["docker", "build", "-t", tag, path]
+        process = subprocess.Popen(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True
+        )
+
+        # Print build logs in real-time
+        for line in process.stdout:
+            print(line, end='')
+
+        # Wait for the build process to complete
+        process.wait()
+
+        # Check the exit code
+        if process.returncode == 0:
+            print("SUCCESS: Image build completed successfully!")
+        else:
+            print("FAILURE: Image build failed!")
+
+    # Usage example
+    print('INFO: building processor image...')
+    print('===============================')
+    print('===============================')
+    stream_build_logs(processor_path, 'quick_batch_processor_app')
+    print('===============================')
+    print('===============================')
+
+    
+    # try:
+    #     # Set quiet=False to print the build output
+    #     build_output = client.images.build(path=processor_path, tag='quick_batch_processor_app',
+    #                     quiet=False)
+            
+    #     # Print the build output
+    #     for line in build_output[1]:
+    #         if 'stream' in line:
+    #             print(line['stream'], end='')
+    #             sys.stdout.flush() 
+
+    #     # Build completed successfully
+    #     print("Image build completed!")
+        
+    # except docker.errors.BuildError as e:
+    #     # Handle build errors
+    #     print("Build failed!")
+    #     print("Error message:", e.msg)
+        
+    # except docker.errors.APIError as e:
+    #     # Handle API errors
+    #     print("API error occurred during build!")
+    #     print("Error message:", e)
+
+    # except Exception as e:
+    #     # Handle other exceptions
+    #     print("An unexpected error occurred during build:", str(e))
+        
+        
+        
+        
+    # Build completed
+    print("INFO: ... processor image build completed!")
 
     # remove processor_requirements.txt from the processor_path directory
     os.remove(container_path)
