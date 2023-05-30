af2a0ee4bf4fef9224ed067193188a4cad1d448f
Jeremy Watt <jermwatt@gmail.com>
2023-05-30 16:33:54 -0700

    scaler added to ux

--- quick_batch/main.py
+++ quick_batch/main.py
@@ -11,12 +11,15 @@ def main(config=None):
         if sys.argv[1] == 'config':
             config = sys.argv[2]
             runner.run(config)
+            return None
         elif sys.argv[1] == 'scale':
             num_processors = int(sys.argv[2])
             manual_scaler.scaler(num_processors)
+            return None
 
     # check config - used as script fire will be used
     runner.run(config)
+    return None
 
 
 if __name__ == '__main__':
